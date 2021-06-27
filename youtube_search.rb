940d46024a0b1432da1260868d0afaf43f098181
sekiguchi0215 <dsphere5296@gmail.com>
2021-06-27 17:15:23 +0900

    検索機能を実装

--- youtube_search.rb
+++ youtube_search.rb
@@ -6,4 +6,22 @@ youtube.key = ENV["GOOGLE_API_KEY"]
 youtube_search_list = youtube.list_searches('id,snippet', type: 'video', q: 'プログラミング', max_results: 10)
 puts youtube_search_list
 
-#コミットするためのコメント
+text = ""
+
+youtube_search_list.items.each do |item|
+  channel_title = item.snippet.channel_title
+  video_title = item.snippet.title
+  video_id = item.id.video_id
+
+  text +=<<~EOS
+
+  チャンネルタイトル:#{channel_title}
+  タイトル:#{video_title}
+  URL:https://www.youtube.com/watch?v=#{video_id}
+  iframe:<iframe width="560" height="315" src="https://www.youtube.com/embed/#{video_id}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+  EOS
+
+end
+
+puts text
