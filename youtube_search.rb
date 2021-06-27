1dd10be501b6515cdae9c348a4de8f938baddab0
sekiguchi0215 <dsphere5296@gmail.com>
2021-06-27 16:45:31 +0900

    エラー発生中

--- youtube_search.rb
+++ youtube_search.rb
@@ -3,3 +3,5 @@ require "google/apis/youtube_v3"
 
 youtube = Google::Apis::YoutubeV3::YouTubeService.new
 youtube.key = ENV["GOOGLE_API_KEY"]
+youtube_search_list = youtube.list_searches('id,snippet', type: 'video', q: 'プログラミング', max_results: 10)
+puts youtube_search_list
